ba352fb0caa0aac09ed5ffdebfd9fcb6488bd172
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-06-12 19:38:22 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+	{
+		if (n != 'e' && n != 'q')
+			putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
