6fe8d058350d03faaa75a568391824f5dbdcb2bb
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-06-12 12:25:11 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for(int n = 'a'; n <= 'z' ; n++)
+		{
+			putchar(n);
+		}
+	return(0);
+}
