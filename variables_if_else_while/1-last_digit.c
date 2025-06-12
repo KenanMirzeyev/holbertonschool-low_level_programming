ee947d60f54b5b11e790bb741608b5b0331eb4aa
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-06-12 11:16:12 +0000

    1-last_digit.c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5)
+	{
+		printf("
+	return (0);
+}
