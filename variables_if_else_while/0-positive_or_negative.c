c79758a5fbc3956e896072f8d0e96ca7034ce14b
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-06-12 10:15:33 +0000

    0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* main
+*
+**positive_or_negative
+*
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* is_positive
+	*
+	** is_zero
+	*
+	** is_negative
+	*
+	*/
+	return(0);
+}
