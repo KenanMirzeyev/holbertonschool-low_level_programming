d013c660773da3d9f75e7b9427319f5aed72d7f3
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-06-18 08:12:33 +0000

    1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+/**
+ * _isdigit - Entry point
+ * @c: check
+ *
+ * Return: Always 0 or 1
+ */
 int _isdigit(int c)
 {
-	if(c == '0' &&  c <= '9')
+	if (c >= '0' &&  c <= '9')
 	{
 		return (1);
 		c++;
-	} else {
+	}
+
+	else
+
+	{
 		return (0);
 	}
 }
