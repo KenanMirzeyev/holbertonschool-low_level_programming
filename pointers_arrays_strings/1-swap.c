2b9a5dca5337ba051c766f288a7ac425eb2db405
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-06-20 14:21:54 +0000

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swapping 2 int
+ * @a: n
+ * @b: i
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
+
