da0a76af2e8fb235f1c3ada779b840e56df6e10b
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-06-23 15:47:29 +0000

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - factorial
+ * @n: n
+ *
+ * Return: n
+ */
+int factorial(int n)
+{
+	if (n == '\0')
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
+
