54e9a0ee6491daafdb61f3fd5ad720d0fa6b3707
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-06-23 14:54:34 +0000

    8-print_diagsums.c

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - p
+ * @a: a
+ * @size: size
+ *i
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int er = 0;
+	int hi = 0;
+
+
+	for (i = 0 ; i < size ; i++)
+	{
+		er += a[i * size + i];
+		hi += a[i * size + (size - 1 - i)];
+
+	}
+	printf("%d %d\n", er, hi);
+}
