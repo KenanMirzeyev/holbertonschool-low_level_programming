2a7a54f9a352c1f2296e99ad4a8b6c5c17044905
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-06-23 15:37:06 +0000

    2-strlen_recursion.c

---
+++ recursion/2-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+	int _strlen_recursion(char *s);
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
