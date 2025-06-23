d601c5a7ce1d9b3756ba88e3a20270e56da7d2d9
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-06-23 15:29:03 +0000

    1-print_rev_recursion.c

---
+++ recursion/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	void _print_rev_recursion(char *s);
+
+	_print_rev_recursion("\nColton Walker");
+	return (0);
+}
