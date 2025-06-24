ba2e078a79985dabffe7c3df7c2e28722b1b299d
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-06-24 14:27:36 +0000

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,18 +10,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, l;
+	int i, j = 0, l;
 
-	if (argc != 3)
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	i = atoi(argv[1]);
-	j = atoi(argv[2]);
-	l = i + j;
-
-	printf("%d\n", l);
+	for (i = 1 ; i < argc ; i++)
+	{
+		l = atoi(argv[i]);
+		if (l > 0)
+		{
+			j += l;
+		}
+	}
+	printf("%d\n", j);
 	return (0);
 }
