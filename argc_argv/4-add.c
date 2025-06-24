e240a686dee3f4b730e3163327bb2f6dfdd2f489
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-06-24 14:20:39 +0000

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main
+ * @argc: ar
+ * @argv: ar
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, l;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	l = i + j;
+
+	printf("%d\n", l);
+	return (0);
+}
