24e42679476c6b34f45bc4a9d85edf6f359a5ba5
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-07-05 18:00:05 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure that holds information about a dog
+ * @name: Name of the dog (string)
+ * @age: Age of the dog (float)
+ * @owner: Name of the dog's owner (string)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
