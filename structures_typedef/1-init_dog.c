d4bdf7b3628db27d0d67da86f705b1b73d8393b8
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-07-05 18:08:52 +0000

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * init_dog - dog
+ * @d: d
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: r
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	d ->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
