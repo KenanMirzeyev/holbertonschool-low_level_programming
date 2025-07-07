0e3735ebff98360488299f91d26fcf869bd8a332
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-07-07 19:20:57 +0000

    1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - array
+ * @array: array
+ * @size: size
+ * @actoin: action
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	
+	if (array == NULL && action == NULL)
+	{
+		return;
+	}
+	for (i = 0 ; i < size ; i++)
+	{
+		action(array[i]);
+	}
+}
+
