476aba39f75daaf38095e3686b44549ca013fb08
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-07-08 18:53:13 +0000

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,43 +2,58 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+/**
+ * print_char - Prints a char from the argument list
+ * @args: Pointer to va_list
+ * @sep: Separator string to print before the value
+ */
 void print_char(va_list *args, char *sep)
 {
 	printf("%s%c", sep, va_arg(*args, int));
 }
 
+/**
+ * print_int - Prints an int from the argument list
+ * @args: Pointer to va_list
+ * @sep: Separator string to print before the value
+ */
 void print_int(va_list *args, char *sep)
 {
 	printf("%s%d", sep, va_arg(*args, int));
 }
 
+/**
+ * print_float - Prints a float from the argument list
+ * @args: Pointer to va_list
+ * @sep: Separator string to print before the value
+ */
 void print_float(va_list *args, char *sep)
 {
 	printf("%s%f", sep, va_arg(*args, double));
 }
 
+/**
+ * print_string - Prints a string from the argument list
+ * @args: Pointer to va_list
+ * @sep: Separator string to print before the value
+ */
 void print_string(va_list *args, char *sep)
 {
 	char *str = va_arg(*args, char *);
+
 	if (!str)
 		str = "(nil)";
 	printf("%s%s", sep, str);
 }
-
 /**
- * print_all - prints anything
- * @format: list of types of arguments
+ * print_all - Prints any combination of char, int, float, and string
+ * @format: A string of format specifiers (c, i, f, s)
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0, j;
 	char *separator = "";
-
-	struct print_type {
-		char symbol;
-		void (*print_func)(va_list *args, char *sep);
-	};
 
 	struct print_type types[] = {
 		{'c', print_char},
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/**
+ * struct print_type - Struct for format symbol and corresponding function
+ * @symbol: Format character (e.g., 'c', 'i', 'f', 's')
+ * @print_func: Pointer to function that prints the argument
+ */
+struct print_type
+{
+	char symbol;
+	void (*print_func)(va_list *args, char *sep);
+};
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
