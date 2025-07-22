169e0a006fd7fbaeb146561d6610239be75d5bfe
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-07-22 17:37:32 +0000

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints the line
+ * @h: h
+ *
+ * Return s
+ */
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		
+		h = h->next;
+		s++;
+	}
+	return (s);
+}
+
