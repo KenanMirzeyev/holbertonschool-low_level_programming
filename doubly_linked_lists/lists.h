1577511ceb61b02d5843bd7e62223ccf3632c8b8
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-07-23 16:21:13 +0000

    2-add_dnodeint.c

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -18,6 +20,7 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 size_t dlistint_len(const dlistint_t *h);
 
 #endif
