0c12749f8dd632da229e440cf34ebe0fe66ad7a3
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-07-24 16:40:09 +0000

    8-delete_dnodeint.c

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - delete
+ * @head: h
+ * @index: idx
+ *
+ * Return: new_node
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *c;
+	unsigned int a = 0;
+
+	if (head == NULL || *head == NULL)
+	{
+		return (-1);
+	}
+
+	c = *head;
+
+	if (index == 0)
+	{
+		*head = c->next;
+		if (head != NULL)
+		{
+			(*head)->prev = NULL;
+		}
+		free(c);
+		return (1);
+	}
+
+	while ( c != NULL && a < index)
+	{
+		c = c->next;
+		a++;
+	}
+
+	while (c == NULL)
+	{
+		return (1);
+	}
+	if (c->prev != NULL)
+	{
+		c->prev->next = c->next;
+	}
+	if (c->next != NULL)
+	{
+		c->next->prev = c->prev;
+	}
+
+	free(c);
+	return (1);
+}
+
+
+
