9bdaafd726a6f12d8e6b437ba6cc2614c6abbae4
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2025-07-24 16:45:59 +0000

    .

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -14,14 +14,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int a = 0;
 
 	if (head == NULL || *head == NULL)
-		return (1);
+		return (-1);
 
 	c = *head;
 
 	if (index == 0)
 	{
 		*head = c->next;
-		if (head != NULL)
+		if (*head != NULL)
 			(*head)->prev = NULL;
 		free(c);
 		return (1);
