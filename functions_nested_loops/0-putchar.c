dfb47992e56c7f5a1001fecebda976275a0d7f5e
KenanMirzeyev <aida-mirzayeva.87@mail.ru>
2026-01-04 12:47:01 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,16 +1,21 @@
 #include "main.h"
 
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	char *str = "_putchar\n";
-	int i = 0;
-
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
-
